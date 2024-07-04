97f55be418ad4e6ca15a3a04c3073050a88a7a30
StudiousXiaoYu <1316356098@qq.com>
2024-07-04 10:57:35 +0800

    chat init

---
+++ src/main/java/com/example/demo/chatClient/MyController.java
@@ -0,0 +1,23 @@
+package com.example.demo.chatClient;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class MyController {
+
+    private final ChatClient chatClient;
+
+    public MyController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+            .user(userInput)
+            .call()
+            .content();
+    }
+}
---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 访问：http://localhost:9149/doc.html
+ * 或：http://localhost:9149/swagger-ui/index.html
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("spring-ai-demo")
+                        .contact(new Contact())
+                        .description("努力的小雨-API文档")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("外部文档")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
